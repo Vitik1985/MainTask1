3480822668aed5d6ebb9c37d9a1dc77d51a235b8
Vitik1985 <viktar.vashko@gmai.com>
2021-09-29 00:09:45 +0300

    Initial commit

---
+++ src/MainTask1.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MainTask1 {
+public  static void main(String[] args) throws Exception{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String name_user;
+name_user = br.readLine();
+    System.out.println("Hello, " + name_user + "!");
+  }
+}
